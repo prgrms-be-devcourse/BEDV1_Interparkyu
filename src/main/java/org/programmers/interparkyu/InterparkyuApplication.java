1704a8c7813c3854f5498f44b5a616c5b7e135ff
Sangmin Lee <sangminlee7648@gmail.com>
2021-10-27 08:26:07 +0900

    project setup

---
+++ src/main/java/org/programmers/interparkyu/InterparkyuApplication.java
@@ -0,0 +1,13 @@
+package org.programmers.interparkyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterparkyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterparkyuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/programmers/interparkyu/InterparkyuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.programmers.interparkyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterparkyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
