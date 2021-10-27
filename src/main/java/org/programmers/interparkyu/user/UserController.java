762c7494a0e4eae770118f0ce2dd358650898fbf
sangminlee <sangminlee7648@gmail.com>
2021-10-28 04:17:45 +0900

    [TI-21] Feature:  User 이름 50자 제한 검증 추가

--- src/main/java/org/programmers/interparkyu/user/UserController.java
+++ src/main/java/org/programmers/interparkyu/user/UserController.java
@@ -1,9 +1,14 @@
 package org.programmers.interparkyu.user;
 
+import javax.validation.ConstraintViolationException;
 import lombok.AllArgsConstructor;
 import org.programmers.interparkyu.ApiResponse;
 import org.programmers.interparkyu.user.dto.request.CreateUserRequest;
 import org.programmers.interparkyu.user.dto.response.UserIdResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +21,21 @@ public class UserController {
 
     private UserService userService;
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiResponse<String> invalidRequestHandler(MethodArgumentNotValidException e) {
+        return ApiResponse.fail(e.getMessage(), "someuri", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ApiResponse<String> invalidRequestHandler(ConstraintViolationException e) {
+        return ApiResponse.fail(e.getMessage(), "someuri", HttpStatus.BAD_REQUEST);
+    }
+
     @PostMapping("/users")
-    public ApiResponse<UserIdResponse> createUser(@RequestBody CreateUserRequest request) {
+    public ApiResponse<UserIdResponse> createUser(
+        @Validated @RequestBody
+        CreateUserRequest request
+    ) {
         Long userId = userService.saveUser(request);
         return ApiResponse.ok(
             "/v1/users",
