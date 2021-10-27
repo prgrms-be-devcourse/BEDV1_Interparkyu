45de23701d84e296e39d38059885e5bff30e4ade
HwiNyeon Kim <z2b23173us@gmail.com>
2021-10-27 22:51:01 +0900

    [TI-67] Rafactor:  필드 사이 개행 추가 피드백 반영

--- src/main/java/org/programmers/interparkyu/ApiResponse.java
+++ src/main/java/org/programmers/interparkyu/ApiResponse.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.programmers.CommonData;
 
 @Getter
 @NoArgsConstructor
--- src/main/java/org/programmers/interparkyu/CommonData.java
+++ src/main/java/org/programmers/interparkyu/CommonData.java
@@ -1,4 +1,4 @@
-package org.programmers;
+package org.programmers.interparkyu;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
@@ -8,9 +8,12 @@ import lombok.Getter;
 @Getter
 public class CommonData {
     private final String message;
+
     private final String requestUri;
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMMddHHmmss", timezone = "Asia/Seoul")
     private final LocalDateTime timestamp = LocalDateTime.now();
+
     private final Integer internalHttpStatusCode;
 
     @Builder
