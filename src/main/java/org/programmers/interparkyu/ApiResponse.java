d7cd4fff0334b91a67f7b400f3904fa70a2eabca
HwiNyeon Kim <z2b23173us@gmail.com>
2021-10-27 22:40:31 +0900

    [TI-67] Feature:  공통 응답용 API Response 추가

---
+++ src/main/java/org/programmers/interparkyu/ApiResponse.java
@@ -0,0 +1,29 @@
+package org.programmers.interparkyu;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.programmers.CommonData;
+
+@Getter
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private CommonData common;
+    private List<T> data = new ArrayList<>();
+
+    public ApiResponse(CommonData common, T data) {
+        this.common = common;
+        this.data.add(data);
+    }
+
+    public static <T> ApiResponse<T> ok(String requestUri, T data) {
+        CommonData common = new CommonData("success", requestUri, 200);
+        return new ApiResponse<>(common, data);
+    }
+
+    public static <T> ApiResponse<T> fail(String failMessage, String requestUri, T data, Integer statusCode) {
+        CommonData common = new CommonData(failMessage, requestUri, statusCode);
+        return new ApiResponse<>(common, data);
+    }
+}
---
+++ src/main/java/org/programmers/interparkyu/CommonData.java
@@ -0,0 +1,22 @@
+package org.programmers;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class CommonData {
+    private final String message;
+    private final String requestUri;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMMddHHmmss", timezone = "Asia/Seoul")
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final Integer internalHttpStatusCode;
+
+    @Builder
+    public CommonData(String message, String requestUri, Integer statusCode) {
+        this.message = message;
+        this.requestUri = requestUri;
+        this.internalHttpStatusCode = statusCode;
+    }
+}
