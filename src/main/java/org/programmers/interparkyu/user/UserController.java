8d0646efd2aa8bff3709c708ab1ff8915b3baa93
sangminlee <sangminlee7648@gmail.com>
2021-10-28 18:11:24 +0900

    [TI-22] Chore: rebase 충돌 해결

--- src/main/java/org/programmers/interparkyu/user/UserController.java
+++ src/main/java/org/programmers/interparkyu/user/UserController.java
@@ -1,16 +1,13 @@
 package org.programmers.interparkyu.user;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.programmers.interparkyu.ApiResponse;
 import org.programmers.interparkyu.user.dto.response.UserResponse;
 import org.programmers.interparkyu.user.dto.request.CreateUserRequest;
 import org.programmers.interparkyu.user.dto.response.UserIdResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +20,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/users")
+    @PostMapping
     public ApiResponse<UserIdResponse> createUser(
         @Valid @RequestBody
         CreateUserRequest request
