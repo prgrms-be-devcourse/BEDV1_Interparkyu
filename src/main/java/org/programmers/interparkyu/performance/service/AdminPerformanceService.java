f1d99f4ea7c6dcab44cad5a37c66289ee1327005
0923kdh <0923kdh@gmail.com>
2021-10-29 11:57:39 +0900

    [TI-16] Chore:  코드 리뷰 적용

--- src/main/java/org/programmers/interparkyu/performance/service/AdminPerformanceService.java
+++ src/main/java/org/programmers/interparkyu/performance/service/AdminPerformanceService.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @AllArgsConstructor
-@Transactional
 @Service
 public class AdminPerformanceService {
 
