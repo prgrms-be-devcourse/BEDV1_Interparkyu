f10fa99f8296fd0a5eeb9a280ee5a4e9be7e66eb
HwiNyeon Kim <z2b23173us@gmail.com>
2021-10-29 16:14:34 +0900

    [TI-25] Feature:  특정 공연 탐색시 없는 공연일 경우 NotFoundException을 반환하도록 수정

--- src/main/java/org/programmers/interparkyu/performance/service/UserPerformanceService.java
+++ src/main/java/org/programmers/interparkyu/performance/service/UserPerformanceService.java
@@ -3,6 +3,7 @@ package org.programmers.interparkyu.performance.service;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.programmers.interparkyu.error.exception.NotFoundException;
 import org.programmers.interparkyu.performance.Performance;
 import org.programmers.interparkyu.performance.repository.UserPerformanceRepository;
 import org.programmers.interparkyu.performance.dto.BriefPerformanceInfo;
@@ -28,9 +29,8 @@ public class UserPerformanceService {
 
     @Transactional(readOnly = true)
     public PerformanceSummary getPerformanceById(Long performanceId) {
-        // TODO 2020.10.28 TI-25 : TI-72번(Global Exception 처리) 완료되면 잘못된 공연 요청에 대한 예외를 던지도록 수정
         Performance performance = repository.findById(performanceId)
-            .orElseThrow(() -> new RuntimeException("No such performance exist"));
+            .orElseThrow(() -> new NotFoundException("No such performance exist"));
         return PerformanceSummary.from(performance);
     }
 }
