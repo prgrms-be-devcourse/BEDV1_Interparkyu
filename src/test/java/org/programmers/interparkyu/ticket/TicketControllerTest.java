81ea9d4aee0406917135d19b7268a359e6948c68
Sangmin Lee <sangminlee7648@gmail.com>
2021-11-02 08:38:34 +0900

    [TI-85] Test: 예매 티켓 생성 테스트 추가

--- src/test/java/org/programmers/interparkyu/ticket/TicketControllerTest.java
+++ src/test/java/org/programmers/interparkyu/ticket/TicketControllerTest.java
@@ -1,37 +1,82 @@
 package org.programmers.interparkyu.ticket;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.programmers.interparkyu.ReservationStatus;
+import org.programmers.interparkyu.RoundSeat;
+import org.programmers.interparkyu.roundSeat.RoundSeatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-@ActiveProfiles("test")
 class TicketControllerTest {
 
-  @Autowired
-  private TicketService ticketService;
+    @Autowired
+    private TicketService ticketService;
 
-  @Autowired
-  private MockMvc mockMvc;
+    @Autowired
+    private RoundSeatService roundSeatService;
 
-  @Autowired
-  private ObjectMapper objectMapper;
+    @Autowired
+    private MockMvc mockMvc;
 
-  @Test
-  @DisplayName("티켓 1개 정보를 가져올 수 있다.")
-  public void getReservationTicketDetail() throws Exception {
+    @Autowired
+    private ObjectMapper objectMapper;
 
-    //To do
-    //ticket create Api 만들어진 후에 작성 필요
+    @Test
+    @DisplayName("티켓 1개 정보를 가져올 수 있다.")
+    public void getReservationTicketDetail() throws Exception {
 
-  }
+        //To do
+        //ticket create Api 만들어진 후에 작성 필요
+
+    }
+
+    @Test
+    @DisplayName("티켓을 생성할 수 있다")
+    @Transactional
+    public void testCreateTicket() throws Exception {
+        // Given
+        Long roundSeatId = 1L;
+        CreateTicketRequest request = new CreateTicketRequest(roundSeatId, 1L);
+
+        // When
+        MvcResult result = mockMvc
+            .perform(
+                post("/v1/tickets")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request))
+            )
+            .andExpect(
+                MockMvcResultMatchers
+                    .jsonPath("$.common.internalHttpStatusCode")
+                    .value(200)
+            )
+            .andReturn();
+
+        // Then
+        String ticketId = JsonPath
+            .parse(result.getResponse().getContentAsString())
+            .read("$.data.ticketId", String.class);
+
+        ticketService.getReservationTicketDetail(ticketId);
+
+        RoundSeat roundSeat = roundSeatService.getRoundSeatById(roundSeatId);
+        assertThat(roundSeat.getReservationStatus(), equalTo(ReservationStatus.WAITING_FOR_PAYMENT));
+    }
 
 }
